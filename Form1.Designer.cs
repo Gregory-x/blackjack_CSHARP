9b136cd55424501d5159e8d7fe89b3c7fe29f8fa
Greg <78381908+Daniel1youdog@users.noreply.github.com>
2023-04-20 11:19:17 +0300

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace BlackJackV1
+﻿namespace WindowsFormsApplication1
 {
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,228 +23,40 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.pictureBox7 = new System.Windows.Forms.PictureBox();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox1.Location = new System.Drawing.Point(381, 382);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox2.Location = new System.Drawing.Point(438, 382);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Visible = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox3.Location = new System.Drawing.Point(494, 382);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Visible = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox4.Location = new System.Drawing.Point(552, 382);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Visible = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox5.Location = new System.Drawing.Point(552, 108);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox5.TabIndex = 7;
-            this.pictureBox5.TabStop = false;
-            this.pictureBox5.Visible = false;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox6.Location = new System.Drawing.Point(494, 108);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox6.TabIndex = 6;
-            this.pictureBox6.TabStop = false;
-            this.pictureBox6.Visible = false;
-            // 
-            // pictureBox7
-            // 
-            this.pictureBox7.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox7.Location = new System.Drawing.Point(438, 108);
-            this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox7.TabIndex = 5;
-            this.pictureBox7.TabStop = false;
-            this.pictureBox7.Visible = false;
-            // 
-            // pictureBox8
-            // 
-            this.pictureBox8.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.pictureBox8.Location = new System.Drawing.Point(381, 108);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(80, 140);
-            this.pictureBox8.TabIndex = 4;
-            this.pictureBox8.TabStop = false;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(361, 570);
+            this.button1.Location = new System.Drawing.Point(148, 116);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 50);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Stand";
-            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Size = new System.Drawing.Size(117, 96);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Chartreuse;
-            this.button2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.Location = new System.Drawing.Point(549, 570);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 50);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Hit";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.DarkGreen;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Broadway", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.ForeColor = System.Drawing.Color.Brown;
-            this.textBox1.Location = new System.Drawing.Point(381, 298);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(268, 78);
-            this.textBox1.TabIndex = 10;
-            this.textBox1.Text = "BLACKJACK PAYS 3 TO 2";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.DarkGreen;
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Font = new System.Drawing.Font("Broadway", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.ForeColor = System.Drawing.Color.Brown;
-            this.textBox2.Location = new System.Drawing.Point(466, 551);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(79, 48);
-            this.textBox2.TabIndex = 11;
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox2.TextChanged += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.BackColor = System.Drawing.Color.DarkGreen;
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Font = new System.Drawing.Font("Broadway", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.ForeColor = System.Drawing.Color.Brown;
-            this.textBox3.Location = new System.Drawing.Point(466, 254);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(79, 48);
-            this.textBox3.TabIndex = 12;
-            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkGreen;
-            this.ClientSize = new System.Drawing.Size(1117, 690);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(498, 465);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox7);
-            this.Controls.Add(this.pictureBox8);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox5;
-        private PictureBox pictureBox6;
-        private PictureBox pictureBox7;
-        private PictureBox pictureBox8;
-        private Button button1;
-        private Button button2;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
+
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,10 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using static BlackJackV1.Card;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
-namespace BlackJackV1
+namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
@@ -18,211 +16,16 @@ namespace BlackJackV1
             InitializeComponent();
         }
 
-
-        // default settings
-        int count = 0;
-
-
-        private void button2_MouseClick(object sender, MouseEventArgs e)
-        {
-            //Takes care of the visuals (visible property of the cards)
-            if (count == 0)
-            {
-                pictureBox1.Visible = true;
-                count++;
-            }
-            else if (count == 1)
-            {
-                pictureBox2.Visible = true;
-                //pictureBox7.Visible = true;
-                count++;
-            }
-            else if (count == 2)
-            {
-                pictureBox3.Visible = true;
-                //pictureBox6.Visible = true;
-                count++;
-            }
-            else if (count == 3)
-            {
-                pictureBox4.Visible = true;
-                //pictureBox5.Visible = true;
-                count = 0;
-            }
-
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox2.Text = "kutya";
+            Form2 ablak = new Form2();
+            ablak.ShowDialog();
+            ablak.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
-
-
-    //const int g_minimumDealerScore = 17;
-
-    class Card
-    {
-        public enum Suit
-        {
-            club,
-            diamond,
-            heart,
-            spade,
-
-            max_suits
-        }
-
-        public enum Rank
-        {
-            rank_2,
-            rank_3,
-            rank_4,
-            rank_5,
-            rank_6,
-            rank_7,
-            rank_8,
-            rank_9,
-            rank_10,
-            rank_jack,
-            rank_queen,
-            rank_king,
-            rank_ace,
-
-            max_ranks
-        }
-
-        private Rank m_rank;
-        private Suit m_suit;
-
-        public Card()
-        {
-            m_rank = 0;
-            m_suit = 0;
-        }
-
-        public Card(Rank rank, Suit suit)
-        {
-            m_rank = rank;
-            m_suit = suit;
-        }
-
-        public void Print()
-        {
-            /*switch (m_rank)
-            {
-                case Rank.rank_2: Console.Write('2'); break;
-                case Rank.rank_3: Console.Write('3'); break;
-                case Rank.rank_4: Console.Write('4'); break;
-                case Rank.rank_5: Console.Write('5'); break;
-                case Rank.rank_6: Console.Write('6'); break;
-                case Rank.rank_7: Console.Write('7'); break;
-                case Rank.rank_8: Console.Write('8'); break;
-                case Rank.rank_9: Console.Write('9'); break;
-                case Rank.rank_10: Console.Write('T'); break;
-                case Rank.rank_jack: Console.Write('J'); break;
-                case Rank.rank_queen: Console.Write('Q'); break;
-                case Rank.rank_king: Console.Write('K'); break;
-                case Rank.rank_ace: Console.Write('A'); break;
-                default:
-                    Console.Write('?');
-                    break;
-            }
-
-            switch (m_suit)
-            {
-                case Suit.club: Console.Write('C'); break;
-                case Suit.diamond: Console.Write('D'); break;
-                case Suit.heart: Console.Write('H'); break;
-                case Suit.spade: Console.Write('S'); break;
-                default:
-                    Console.Write('?');
-                    break;
-            }*/
-        }
-
-        public int Value()
-        {
-            switch (m_rank)
-            {
-                case Rank.rank_2: return 2;
-                case Rank.rank_3: return 3;
-                case Rank.rank_4: return 4;
-                case Rank.rank_5: return 5;
-                case Rank.rank_6: return 6;
-                case Rank.rank_7: return 7;
-                case Rank.rank_8: return 8;
-                case Rank.rank_9: return 9;
-                case Rank.rank_10: return 10;
-                case Rank.rank_jack: return 10;
-                case Rank.rank_queen: return 10;
-                case Rank.rank_king: return 10;
-                case Rank.rank_ace: return 11;
-                default:
-                    throw new Exception("should never happen");
-            }
-        }
-    }
-
-    class Deck
-    {
-        private List<Card> m_deck;
-        private int m_cardIndex = 0;
-
-        public Deck()
-        {
-            m_deck = new List<Card>();
-
-            for (int suit = 0; suit < Card.max_suits; ++suit)
-            {
-                for (int rank = 0; rank < Card.max_ranks; ++rank)
-                {
-                    m_deck.Add(new Card((Card.Rank)rank, (Card.Suit)suit));
-                }
-            }
-        }
-
-        public void Print()
-        {
-            foreach (Card card in m_deck)
-            {
-                card.Print();
-                Console.Write(" ");
-            }
-
-            Console.WriteLine();
-        }
-
-        public void Shuffle()
-        {
-            Random rng = new Random();
-            int n = m_deck.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                Card card = m_deck[k];
-                m_deck[k] = m_deck[n];
-                m_deck[n] = card;
-            }
-
-            m_cardIndex = 0;
-        }
-
-        public Card DealCard()
-        {
-            if (m_cardIndex >= m_deck.Count)
-            {
-                throw new InvalidOperationException("No more cards in the deck.");
-            }
-
-            return m_deck[m_cardIndex++];
-        }
-    }
-
-
-
-
-
 }
